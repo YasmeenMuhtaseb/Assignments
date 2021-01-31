5c3b5a07c3e47127b862600b0bf2d951d8161d93
YasmeenMuhtaseb <y.muhtaseb95@gmail.com>
2021-01-31 20:55:53 +0200

    Hook Form

---
+++ MERN-Stack/MERN-React/FunctionalComponents/HookForm/hook_form/src/components/UserForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from  'react';
+    
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+    const [confirmPassword, setConfirmPassword] = useState("");  
+    
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstName, lastName, email, password, confirmPassword};
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <div>
+            <form onSubmit={ createUser }>
+                <div>
+                    <label>First Name </label> 
+                    <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Last Name </label> 
+                    <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Email </label> 
+                    <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                </div>
+                <div>
+                    <label>Password </label>
+                    <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label>Confim Password </label>
+                    <input type="password" onChange={ (e) => setConfirmPassword(e.target.value) } />
+                </div>
+                <input type="submit" value="Create User" />
+            </form>
+            <div>
+                <h3>Your Form Data</h3>
+                <p>First Name {firstName}</p>
+                <p>Last Name {lastName}</p>
+                <p>Email {email}</p>
+                <p>Password {password}</p>
+                <p>Confim Password {confirmPassword}</p>
+            </div>
+        </div>
+    );
+};
+    
+export default UserForm;
