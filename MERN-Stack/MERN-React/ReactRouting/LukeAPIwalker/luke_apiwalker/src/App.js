63bbc01a7ebb21d7c9ec4fd37d4ddc1cd895f3cd
YasmeenMuhtaseb <y.muhtaseb95@gmail.com>
2021-02-06 14:10:04 +0200

    Luke APIwalker

---
+++ MERN-Stack/MERN-React/ReactRouting/LukeAPIwalker/luke_apiwalker/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {Router,navigate} from '@reach/router';
+import Root from './components/Root';
+import Planet from './components/Planet';
+import People from './components/People';
+import {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [input,setInput] = useState('');
+  const dataHandler= (data,id)=>{
+    axios.get(`https://swapi.dev/api/${data}/${id}/`).then(response => {setInput(response.data); navigate(`/${data}/${id}`);})
+  }
+  return (
+    <div className="App">
+      <Root data={dataHandler}/>
+      <Router>
+        <Planet displayData={input} path="/planets/:id/" />
+        <People displayData={input} path="/people/:id/" />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
